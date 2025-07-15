00cd5535a7748f955a98f140c0d0a32e391cce7c
Andreas Schlapsi <andreas@schlapsi.net>
2025-07-15 23:39:26 +0200

    Add flag

--- install/05-tlp.sh
+++ install/05-tlp.sh
@@ -1,6 +1,6 @@
 # Setup TLP
 sudo pacman -S --noconfirm --needed tlp
-sudo rm /etc/tlp.d/00-customization.conf
+sudo rm -f /etc/tlp.d/00-customization.conf
 sudo ln -s $KNARCHY_DIR/config/tlp/00-customization.conf /etc/tlp.d/00-customization.conf
 
 sudo systemctl enable --now tlp.service
