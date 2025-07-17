91b8cfc478c6b5a93186ec8f3b71b2050fc7bbd4
Andreas Schlapsi <andreas@schlapsi.net>
2025-07-17 08:19:16 +0200

    Moved directory for backgrounds

--- install/hyprland.sh
+++ install/hyprland.sh
@@ -6,11 +6,21 @@ yay -S --noconfirm --needed \
 # Start Hyprland on first session
 #echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec Hyprland" >~/.bash_profile
 
+rm -rf $HOME/.config/backgrounds/
+mkdir -p $HOME/.config/backgrounds/
+cp -R $KNARCHY_DIR/config/backgrounds/* $HOME/.config/backgrounds/
+
 rm -rf $HOME/.config/hypr
-rm -rf $HOME/.config/waybar
+mkdir -p $HOME/.config/hypr
 cp -R $KNARCHY_DIR/config/hyprland/* $HOME/.config/hypr/
+
+rm -rf $HOME/.config/waybar
+mkdir -p $HOME/.config/waybar
 cp -R $KNARCHY_DIR/config/waybar/* $HOME/.config/waybar/
+
 # Set defaults
-ln -s $HOME/.config/hypr/backgrounds/solitude.jpg $HOME/.config/hypr/backgrounds/wallpaper.jpg
+ln -s $HOME/.config/backgrounds/solitude.jpg $HOME/.config/backgrounds/wallpaper.jpg
 ln -s $HOME/.config/hypr/device-dependent/none.conf $HOME/.config/hypr/device-dependent/current.conf
+
+hyprctl reload
 
