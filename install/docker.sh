bd084570bb5a15ba5887da443eab46998e42ab98
Andreas Schlapsi <andreas@schlapsi.net>
2025-08-07 08:10:10 +0200

    Add packages for Python and JavaScript development

--- install/docker.sh
+++ install/docker.sh
@@ -1,4 +1,4 @@
-yay -S --noconfirm --needed docker docker-compose
+sudo pacman -S --noconfirm --needed docker docker-compose
 
 sudo systemctl enable docker
 
---
+++ install/python.sh
@@ -0,0 +1,1 @@
+sudo pacman -S --noconfirm --needed python uv python-ruff python-black python-isort
